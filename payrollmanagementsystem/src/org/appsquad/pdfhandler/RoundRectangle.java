09dd765450ad9f41204394244ac76b8f97275a73
somnathdutta <somnathdutta048@gmail.com>
2016-08-05 09:56:51 +0530

    #new

---
+++ payrollmanagementsystem/src/org/appsquad/pdfhandler/RoundRectangle.java
@@ -0,0 +1,18 @@
+package org.appsquad.pdfhandler;
+
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPCellEvent;
+import com.itextpdf.text.pdf.PdfPTable;
+
+public class RoundRectangle implements PdfPCellEvent {
+    public void cellLayout(PdfPCell cell, Rectangle rect,
+            PdfContentByte[] canvas) {
+        PdfContentByte cb = canvas[PdfPTable.LINECANVAS];
+        cb.roundRectangle(
+            rect.getLeft() + 1.5f, rect.getBottom() + 1.5f, rect.getWidth() - 3,
+            rect.getHeight() - 3, 4);
+        cb.stroke();
+    }
+}
